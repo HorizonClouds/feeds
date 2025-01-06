99866a9ab503dfa41468e138cf1420c82e2c0540
JaviFdez7 <javidelatorre400@gmail.com>
2025-01-06 03:04:28 +0100

    feat: refactor MongoDB connection logic to improve error handling and support in-memory database for testing

--- src/server.js
+++ src/server.js
@@ -38,31 +38,50 @@ app.use(errorHandler);
 swaggerSetup(app);
 
 // Connect to MongoDB
-let mongoURI = config.mongoUri;
-if (config.nodeEnv === 'test') {
-  const mongod = new MongoMemoryServer(); // Fake MongoDB for testing
-  await mongod.start();
-  mongoURI = mongod.getUri();
-  console.log(mongoURI);
-}
-
-mongoose
-  .connect(mongoURI)
+connectDB()
   .then(() => {
-    logger.info('Connected to MongoDB');
+    // Start server
+    app.listen(port, () => {
+      logger.info(`Server is running on http://localhost:${port}`);
+      logger.info(
+        `API documentation is available at http://localhost:${port}/api-docs`
+      );
+    });
   })
   .catch((error) => {
-    logger.info('Error connecting to MongoDB:', error.message);
+    logger.info('Error connecting to MongoDB:' + error.message);
   });
 
+async function connectDB() {
+  logger.info('Connecting to MongoDB...');
+  let mongoURI = config.mongoUri;
+  let mongod;
 
+  if (process.env.NODE_ENV === 'test') {
+    mongod = await MongoMemoryServer.create();
+    mongoURI = mongod.getUri();
+  }
 
-// Start server
-app.listen(port, () => {
-  logger.info(`Server is running on http://localhost:${port}`);
-  logger.info(
-    `API documentation is available at http://localhost:${port}/api-docs`
-  );
-});
+  try {
+    //timeout 3 seconds
+    await mongoose.connect(mongoURI, { connectTimeoutMS: 3000, serverSelectionTimeoutMS: 5000 });
+    logger.info(`Connected to MongoDB in ${process.env.NODE_ENV === 'test' ? 'test (in-memory)' : 'default'} mode`);
+    //Delete all data in the database when connect (dev purposes)
+    // await mongoose.connection.db.dropDatabase();
+  } catch (error) {
+    console.error('Initial connection failed, retrying with in-memory MongoDB.', error.message);
+    if (!mongod) {
+      mongod = await MongoMemoryServer.create();
+      mongoURI = mongod.getUri();
+    }
+    try {
+      await mongoose.connect(mongoURI);
+      logger.info('Connected to in-memory MongoDB after initial failure');
+    } catch (err) {
+      console.error('Failed to connect to in-memory MongoDB', err);
+    }
+  }
+};
 
 export default app; // Export the Express application
+
